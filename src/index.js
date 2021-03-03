de09bd9fe91ead9d2b2122524dd78ff55b6d00cd
13redfox <sergei181989@gmail.com>
2021-03-03 13:32:57 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+
+    let reverseNum = n.toString().split('').reverse().join('');
+    let num = Math.sign(n) * parseInt(reverseNum);
+
+    if ( n < 0 ) {
+        return num * -1;
+    }
+
+    return num;
+};
